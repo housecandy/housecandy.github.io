cdc55eaaa1c062a1353c9cb938095348f373f253
tdd2533 <tdd2533@msn.cn>
2019-08-16 22:06:09 +0800

    test2

---
+++ test/gumball/mightygumball.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+	var url = "https://housecandy.github.io/test/gumball/sales.json";
+	var request = new XMLHttpRequest();
+	request.open("GET", url);
+	request.onload = function(){
+		if(request.status == 200) {
+			updateSales(request.responseText);
+		}
+	};
+	request.send(null);
+}
+
+function updateSales(responseText) {
+	var salesDiv = document.getElementById("sales");
+	salesDiv.innerHTML = responseText;
+}
