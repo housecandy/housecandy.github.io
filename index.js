6fe2404db84e6cf72ae601984e90801096324aed
tdd2533 <tdd2533@msn.cn>
2019-09-07 09:56:25 +0800

    3

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 window.onload = function() {
+	setTimeout("jsHidden()", 1000);
+}
+
+function jsHidden() {
 	var clearLoading = document.getElementById("page_loading");
 	clearLoading.classList.add("js_hidden");
 }
