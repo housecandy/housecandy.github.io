d161442d57373b31b98413c4305c99c54144bfde
tdd2533 <tdd2533@msn.cn>
2019-09-09 07:00:03 +0800

    gundong

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 window.onload = function() {
-	setTimeout("jsHidden()", 1000);
-}
-
-function jsHidden() {
 	var clearLoading = document.getElementById("page_loading");
 	clearLoading.classList.add("js_hidden");
+	
+	var navList = document.getElementById("nav_list");
+  	if(navList.classList.contains("js_open")) {
+		navList.classList.remove("js_open");
+	}
+    
+    
+    window.onresize = function() {
+    	if (window.innerWidth > 736) {
+    		if(navList.classList.contains("js_open")) {
+				navList.classList.remove("js_open");
+			}
+    	}
+    };
+  
+    
+	var navButton = document.getElementById("nav_btn");
+	navButton.onclick = showNav;
+}
+
+function showNav() {
+	var navList = document.getElementById("nav_list");
+	if(navList.classList.contains("js_open")) {
+		navList.classList.remove("js_open");
+	} else {
+		navList.classList.add("js_open");
+	}
 }
